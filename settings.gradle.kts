7858dbdf78d6c1f36119673b2d04996ae82c1df8
Epic <EpicEbic@outlook.com>
2024-02-12 22:09:24 +0000

    updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,12 +18,11 @@ dependencyResolutionManagement {
     }
 }
 
-findProject(":pineapple-nms:api")?.name = "api"
-findProject(":pineapple-nms:spigot-v1_20_R3")?.name = "spigot-v1_20_R3"
-findProject(":pineapple-nms:spigot-v1_20_R2")?.name = "spigot-v1_20_R2"
-findProject(":pineapple-nms:spigot-v1_20_R1")?.name = "spigot-v1_20_R1"
 
 include("pineapple-utils", "pineapple-nms", "pineapple-nms:api", "pineapple-core")
-include("pineapple-nms:spigot-v1_20_R3")
-include("pineapple-nms:spigot-v1_20_R2")
-include("pineapple-nms:spigot-v1_20_R1")
+
+file("pineapple-nms").listFiles()?.forEach { project ->
+    if (project.resolve("build.gradle.kts").exists()) {
+        include(project.name)
+    }
+}
