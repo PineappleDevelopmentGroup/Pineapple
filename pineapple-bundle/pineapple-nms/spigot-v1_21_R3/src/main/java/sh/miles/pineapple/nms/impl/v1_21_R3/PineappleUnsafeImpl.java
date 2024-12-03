0a73e97ac4098002c3c5d9583e598208b0ae142e
Epic <EpicEbic@outlook.com>
2024-12-03 19:36:21 +0000

    update, remove item byte methods

---
+++ pineapple-bundle/pineapple-nms/spigot-v1_21_R3/src/main/java/sh/miles/pineapple/nms/impl/v1_21_R3/PineappleUnsafeImpl.java
@@ -0,0 +1,19 @@
+package sh.miles.pineapple.nms.impl.v1_21_R3;
+
+import org.bukkit.NamespacedKey;
+import org.jetbrains.annotations.NotNull;
+import sh.miles.pineapple.collection.registry.FrozenRegistry;
+import sh.miles.pineapple.collection.registry.RegistryKey;
+import sh.miles.pineapple.nms.api.PineappleUnsafe;
+import sh.miles.pineapple.nms.impl.v1_21_R3.registry.PineappleNmsRegistry;
+
+public class PineappleUnsafeImpl implements PineappleUnsafe {
+
+    @SuppressWarnings("unchecked")
+    @NotNull
+    @Override
+    public <T extends RegistryKey<NamespacedKey>> FrozenRegistry<T, NamespacedKey> getRegistry(final Class<? super T> clazz) {
+        return (FrozenRegistry<T, NamespacedKey>) PineappleNmsRegistry.makeRegistry(clazz);
+    }
+
+}
