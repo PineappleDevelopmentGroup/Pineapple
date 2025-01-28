acce2c9523cc23de6accab3c583924e7703b48a0
Epic <EpicEbic@outlook.com>
2025-01-28 23:50:56 +0000

    fix conflicts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,15 +5,7 @@ gradle.rootProject {
 }
 
 include(
-    "pineapple-common",
-    "pineapple-bundle",
-    "pineapple-infstack",
-    "pineapple-tiles",
-    "pineapple-bundle:pineapple-core",
-    "pineapple-bundle:pineapple-nms",
-    "pineapple-bundle:pineapple-nms:api",
-    "pineapple-bundle:pineapple-nms:spigot-v1_20_R3",
-    "pineapple-bundle:pineapple-nms:spigot-v1_20_R4",
-    "pineapple-bundle:pineapple-nms:spigot-v1_21_R1",
-    "pineapple-bundle:pineapple-nms:spigot-v1_21_R2"
+    "paper-bundle",
+    "paper-bundle:paper-core",
+    "paper-common"
 )
